e240670b2bb77c01edc9855e3b8b7becb47f4b61
student-in-progress <rvarg003@odu.edu>
2026-08-27 16:56:11 -0400

    Add robust fallback credentials to DefaultFirebaseOptions for iOS to prevent crash on startup

--- lib/services/firebase_options.dart
+++ lib/services/firebase_options.dart
@@ -24,28 +24,28 @@ class DefaultFirebaseOptions {
   }
 
   static FirebaseOptions get web => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
-    appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? '',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? 'AIzaSyB9CayWyFJSQ7qHczNaTr7yzVS0LTnfmbc',
+    appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '1:384928842494:web:fe40cc61f152b172235baf',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '384928842494',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'church-usher-app',
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? 'church-usher-app.firebaseapp.com',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'church-usher-app.firebasestorage.app',
   );
 
   static FirebaseOptions get android => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? '',
-    appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? 'AIzaSyAXXuz_KW77Y0KWxhSNQe8Wz2GtfKLWnns',
+    appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '1:384928842494:android:0c62680350696b92235baf',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '384928842494',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'church-usher-app',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'church-usher-app.firebasestorage.app',
   );
 
   static FirebaseOptions get ios => FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
-    appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
-    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? 'AIzaSyAr8LRZ8NH1YVj8Vg-38edNr64FsJFXfGE',
+    appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '1:384928842494:ios:4d47a8737a5bf44d235baf',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '384928842494',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'church-usher-app',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'church-usher-app.firebasestorage.app',
     iosBundleId: 'com.usherapp.usherApp',
   );
 
